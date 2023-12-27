ff1efd74abe0a7825558cd417e23b4e52e5a96c2
Numan Karaaslan <numan4634@hotmail.com>
2023-12-27 15:15:28 +0300

    Update JWTAuthorizationFilter.java

--- SpringJDBCRest/src/main/java/com/example/springjdbcrest/config/JWTAuthorizationFilter.java
+++ SpringJDBCRest/src/main/java/com/example/springjdbcrest/config/JWTAuthorizationFilter.java
@@ -30,27 +30,29 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter
 		// even though some endpoints are freely open, they are authorized
 		// you need a bit more configuration to know which paths requires authorization here
 		String token = req.getHeader("Authorization");
+		String mytoken = null;
 		if (token != null)
 		{
 			try
 			{
 				// get rid of "Bearer " if there is any
-				String mytoken = JWT.require(Algorithm.HMAC512("MY_SECRET_KEY".getBytes())).build().verify(token.replace("Bearer ", "")).getSubject();
-				if (mytoken != null)
-				{
-					// mytoken is username-ROLE_USER,ROLE_ADMIN,ROLE_CONTRIBUTER ...etc
-					String username = mytoken.split("-")[0];
-					List<GrantedAuthority> auth = Arrays.asList(mytoken.split("-")[1].split(",")).stream().map(item -> new SimpleGrantedAuthority(item)).collect(Collectors.toList());
-					UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, auth);
-					SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-					chain.doFilter(req, res);
-				}
+				mytoken = JWT.require(Algorithm.HMAC512("MY_SECRET_KEY".getBytes())).build().verify(token.replace("Bearer ", "")).getSubject();
 			}
 			catch (Exception e)
 			{
+				mytoken = null;
 				res.setStatus(HttpStatus.UNAUTHORIZED.value());
 				res.getWriter().write("Token exception => " + e.getMessage());
 				res.flushBuffer();
+			}
+			if (mytoken != null)
+			{
+				// mytoken is username-ROLE_USER,ROLE_ADMIN,ROLE_CONTRIBUTER ...etc
+				String username = mytoken.split("-")[0];
+				List<GrantedAuthority> auth = Arrays.asList(mytoken.split("-")[1].split(",")).stream().map(item -> new SimpleGrantedAuthority(item)).collect(Collectors.toList());
+				UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username, null, auth);
+				SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+				chain.doFilter(req, res);
 			}
 		}
 		else
