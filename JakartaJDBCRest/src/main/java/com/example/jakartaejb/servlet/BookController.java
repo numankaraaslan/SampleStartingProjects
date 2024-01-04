b64f39d847b6cd47ec8596e3716741dd068a5f88
Numan Karaaslan <numan4634@hotmail.com>
2024-01-05 00:25:20 +0300

    Update BookController.java

--- JakartaJDBCRest/src/main/java/com/example/jakartaejb/servlet/BookController.java
+++ JakartaJDBCRest/src/main/java/com/example/jakartaejb/servlet/BookController.java
@@ -20,7 +20,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.Response.Status;
 
-@Stateless
+@RequestScoped
 @Path(value = "book")
 public class BookController
 {
