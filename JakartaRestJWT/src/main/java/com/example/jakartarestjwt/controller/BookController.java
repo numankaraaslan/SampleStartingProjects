0e9d91465015d809af68ed33dcc20c0d7f85e86d
Numan Karaaslan <numan4634@hotmail.com>
2024-07-23 16:34:33 +0300

    Update BookController.java

--- JakartaRestJWT/src/main/java/com/example/jakartarestjwt/controller/BookController.java
+++ JakartaRestJWT/src/main/java/com/example/jakartarestjwt/controller/BookController.java
@@ -2,35 +2,32 @@ package com.example.jakartarestjwt.controller;
 
 import com.example.jakartarestjwt.model.SystemUser;
 
+// imports are important !!!!
 import jakarta.annotation.security.RolesAllowed;
-import jakarta.enterprise.context.RequestScoped;
-import jakarta.inject.Inject;
-import jakarta.security.enterprise.SecurityContext;
+import jakarta.inject.Singleton;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.SecurityContext;
 
 @Path(value = "/book")
-@RequestScoped
+@Singleton
 public class BookController
 {
-	@Inject
-	SecurityContext securityContext;
-
-	@Inject
-	HttpServletRequest req;
 
 	@GET
 	@Path(value = "admin")
 	@Produces(value = MediaType.APPLICATION_JSON)
 	@RolesAllowed(value = { "ADMIN" })
-	public Response admin()
+	public Response admin(@Context SecurityContext securityContext)
 	{
 		// localhost:8080/JakartaRestJWT/book/admin
-		SystemUser currentUser = (SystemUser) securityContext.getCallerPrincipal();
+		SystemUser currentUser = (SystemUser) securityContext.getUserPrincipal();
 		return Response.ok().entity("Your token has " + currentUser.getAuthorities() + " roles and username is (" + currentUser.getUsername() + ")").build();
 	}
 
@@ -38,10 +35,13 @@ public class BookController
 	@Path(value = "user")
 	@Produces(value = MediaType.APPLICATION_JSON)
 	@RolesAllowed(value = { "USER" })
-	public Response user()
+	public Response user(@Context SecurityContext securityContext, @Context HttpServletRequest request, @Context HttpServletResponse response)
 	{
 		// localhost:8080/JakartaRestJWT/book/user
-		SystemUser currentUser = (SystemUser) securityContext.getCallerPrincipal();
+		System.err.println(request.getRequestURL());
+		// you can make redirects or something
+		System.err.println(response.getStatus());
+		SystemUser currentUser = (SystemUser) securityContext.getUserPrincipal();
 		return Response.ok().entity("Your token has " + currentUser.getAuthorities() + " roles and username is (" + currentUser.getUsername() + ")").build();
 	}
 }
